2be51e56974a829dca2c17f472493e66736b7941
yangyangduo <zhaoyang@vip.163.com>
2013-08-07 16:36:55 +0800

    add speech recognition view

---
+++ SmartHome/SpeechRecognitionView.h
@@ -0,0 +1,13 @@
+//
+//  SpeechRecognitionView.h
+//  SmartHome
+//
+//  Created by Zhao yang on 8/7/13.
+//  Copyright (c) 2013 hentre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SpeechRecognitionView : UIView
+
+@end
---
+++ SmartHome/SpeechRecognitionView.m
@@ -0,0 +1,22 @@
+//
+//  SpeechRecognitionView.m
+//  SmartHome
+//
+//  Created by Zhao yang on 8/7/13.
+//  Copyright (c) 2013 hentre. All rights reserved.
+//
+
+#import "SpeechRecognitionView.h"
+
+@implementation SpeechRecognitionView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+@end
