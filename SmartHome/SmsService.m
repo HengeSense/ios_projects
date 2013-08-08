8d4bae162f2a659ee172114dc4f5eb9788d3b3f4
yangyangduo <zhaoyang@vip.163.com>
2013-08-08 23:48:41 +0800

    service 重构

--- SmartHome/SmsService.m
+++ SmartHome/SmsService.m
@@ -26,24 +26,12 @@
     return self;
 }
 
-- (void)sendMessage:(NSString *)message for:(NSString *)phoneNumber {
+- (void)sendMessage:(NSString *)message for:(NSString *)phoneNumber
+            success:(SEL)s failed:(SEL)f target:(id)t callback:(id)cb {
     [self.client getForUrl:
         [SMS_URL stringByAppendingFormat:@"?un=%@&pwd=%@&mobile=%@&msg=%@",
          SMS_UN, SMS_PWD, phoneNumber,message] acceptType:@"application/xml"
-         success:@selector(sendMessageSuccess:) error:@selector(sendMessageFailed:)
-         for:self callback:nil];
-}
-
-- (void)sendMessageSuccess:(RestResponse *)resp {
-    if(resp.statusCode == 200) {
-        //  resp.body
-    } else {
-        [self sendMessageFailed:resp];
-    }
-}
-
-- (void)sendMessageFailed:(RestResponse *)resp {
-    
+         success:s error:f for:t callback:cb];
 }
 
 @end
