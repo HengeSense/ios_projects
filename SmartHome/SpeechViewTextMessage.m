4b1b084fedcd13def19466551fdac527a64063e0
yangyangduo <zhaoyang@vip.163.com>
2013-08-12 22:16:34 +0800

    bubble style chats view && add sound directory

--- SmartHome/SpeechViewTextMessage.m
+++ SmartHome/SpeechViewTextMessage.m
@@ -15,18 +15,19 @@
 
 #define DEFAULT_FONT @"Helvetica"
 #define MESSAGE_MAX_WIDTH 250
+#define BubbleSpace 10
 
 @implementation SpeechViewTextMessage {
     UIView *messageView;
-
 }
 
 @synthesize textMessage;
 
-
 - (UIView *)viewWithMessage {    
     if(messageView != nil) return messageView;
     if([NSString isEmpty:self.textMessage]) return nil;
+    
+    CGFloat screenWidth = [UIScreen mainScreen].bounds.size.width;
     
     //generated text view
 
@@ -41,21 +42,24 @@
     [paragraph addParagraphWithAttributedString:str lineSpace:2.f textAlign:TEXT_ALIGN_NATURAL
         lineBreakMode:LINE_BREAK_MODE_WORD_WRAPPING paragraphSpace:0.f paragraphSpaceBefore:0.f];
     
-    TextContainerView *textView = [paragraph generatedTextViewWithFrame:CGRectMake(10, 5, MESSAGE_MAX_WIDTH, 0)];
+    TextContainerView *textView =
+        [paragraph generatedTextViewWithFrame:
+         CGRectMake(((self.messageOwner == MESSAGE_OWNER_THEIRS) ? 10 : 5), 5, MESSAGE_MAX_WIDTH, 0)];
     textView.backgroundColor = [UIColor clearColor];
     
     //wrapper text view
 
     UIImageView *backgroundImage = nil;
     if(self.messageOwner == MESSAGE_OWNER_THEIRS) {
-        backgroundImage = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, textView.frame.size.width + 14, textView.frame.size.height + 12)];
+        backgroundImage = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, textView.frame.size.width + 16, textView.frame.size.height + 12)];
         backgroundImage.image = [ImageFactory sharedImageFactory].imageForBubbleTheirs;
     } else if(self.messageOwner == MESSAGE_OWNER_MINE) {
-        backgroundImage = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, textView.frame.size.width + 14, textView.frame.size.height + 12)];
+        backgroundImage = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, textView.frame.size.width + 16, textView.frame.size.height + 12)];
         backgroundImage.image = [ImageFactory sharedImageFactory].imageForBubbleMine;
     }
 
-    messageView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, backgroundImage.frame.size.width, backgroundImage.frame.size.height)];
+    messageView = [[UIView alloc] initWithFrame:
+                   CGRectMake(((self.messageOwner == MESSAGE_OWNER_THEIRS) ? BubbleSpace : (screenWidth - backgroundImage.frame.size.width - BubbleSpace)), 0, backgroundImage.frame.size.width, backgroundImage.frame.size.height)];
     messageView.backgroundColor = [UIColor clearColor];
     [messageView addSubview:backgroundImage];
     [messageView addSubview:textView];
