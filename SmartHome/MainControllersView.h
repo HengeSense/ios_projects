f524dc8fad8cea6fa9c2c2e44fca9d1df738e002
HentreGeneral <hadoop@star-mac-pro.local>
2013-08-12 10:16:33 +0800

    main

---
+++ SmartHome/MainControllersView.h
@@ -0,0 +1,13 @@
+//
+//  MainControllersView.h
+//  SmartHome
+//
+//  Created by hadoop user account on 12/08/13.
+//  Copyright (c) 2013 hentre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainControllersView : UIView
+
+@end
---
+++ SmartHome/MainControllersView.m
@@ -0,0 +1,31 @@
+//
+//  MainControllersView.m
+//  SmartHome
+//
+//  Created by hadoop user account on 12/08/13.
+//  Copyright (c) 2013 hentre. All rights reserved.
+//
+
+#import "MainControllersView.h"
+
+@implementation MainControllersView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
