5ef628d6e459802834ce01ac8f0051fe7c6923fe
yangyangduo <zhaoyang@vip.163.com>
2013-08-16 11:46:37 +0800

    add account setting view controller

---
+++ SmartHome/AccountSettingViewController.h
@@ -0,0 +1,14 @@
+//
+//  AccountSettingViewController.h
+//  SmartHome
+//
+//  Created by Zhao yang on 8/16/13.
+//  Copyright (c) 2013 hentre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "NavViewController.h"
+
+@interface AccountSettingViewController : NavViewController
+
+@end
---
+++ SmartHome/AccountSettingViewController.m
@@ -0,0 +1,44 @@
+//
+//  AccountSettingViewController.m
+//  SmartHome
+//
+//  Created by Zhao yang on 8/16/13.
+//  Copyright (c) 2013 hentre. All rights reserved.
+//
+
+#import "AccountSettingViewController.h"
+
+@interface AccountSettingViewController ()
+
+@end
+
+@implementation AccountSettingViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+- (void)initDefaults {
+    
+}
+
+- (void)initUI {
+    
+}
+
+@end
