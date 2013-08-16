82e51155dd500abed14ea9289ee9198d5fad952e
HentreGeneral <hadoop@star-mac-pro.local>
2013-08-16 11:26:27 +0800

    弄、

---
+++ SmartHome/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  SmartHome
+//
+//  Created by hadoop user account on 16/08/13.
+//  Copyright (c) 2013 hentre. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface LoginViewController : BaseViewController
+
+@end
