63a16479f7720df4514ee92f362113f408f6c24a
yangyangduo <zhaoyang@vip.163.com>
2013-08-23 16:14:25 +0800

    结构调整

---
+++ SmartHome/VerificationCodeValidationViewController.h
@@ -0,0 +1,13 @@
+//
+//  VerificationCodeValidationViewController.h
+//  SmartHome
+//
+//  Created by Zhao yang on 8/23/13.
+//  Copyright (c) 2013 hentre. All rights reserved.
+//
+
+#import "NavViewController.h"
+
+@interface VerificationCodeValidationViewController : NavViewController
+
+@end
---
+++ SmartHome/VerificationCodeValidationViewController.m
@@ -0,0 +1,38 @@
+//
+//  VerificationCodeValidationViewController.m
+//  SmartHome
+//
+//  Created by Zhao yang on 8/23/13.
+//  Copyright (c) 2013 hentre. All rights reserved.
+//
+
+#import "VerificationCodeValidationViewController.h"
+
+@interface VerificationCodeValidationViewController ()
+
+@end
+
+@implementation VerificationCodeValidationViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
