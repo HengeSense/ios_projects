9927f1cb61ea020e46f76f08e6f560f2f98ae0d2
yangyangduo <zhaoyang@vip.163.com>
2013-08-30 11:23:36 +0800

    ff

---
+++ SmartHome/DeviceCommandUpdateUnits.h
@@ -0,0 +1,13 @@
+//
+//  DeviceCommandUpdateUnits.h
+//  SmartHome
+//
+//  Created by Zhao yang on 8/30/13.
+//  Copyright (c) 2013 hentre. All rights reserved.
+//
+
+#import "DeviceCommand.h"
+
+@interface DeviceCommandUpdateUnits : DeviceCommand
+
+@end
---
+++ SmartHome/DeviceCommandUpdateUnits.m
@@ -0,0 +1,13 @@
+//
+//  DeviceCommandUpdateUnits.m
+//  SmartHome
+//
+//  Created by Zhao yang on 8/30/13.
+//  Copyright (c) 2013 hentre. All rights reserved.
+//
+
+#import "DeviceCommandUpdateUnits.h"
+
+@implementation DeviceCommandUpdateUnits
+
+@end
