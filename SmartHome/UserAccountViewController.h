90abb878d34a9ed193d7e50bce1aaa01732532f7
HentreGeneral <hadoop@star-mac-pro.local>
2013-09-02 18:43:51 +0800

    user account

---
+++ SmartHome/UserAccountViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserAccountViewController.h
+//  SmartHome
+//
+//  Created by hadoop user account on 2/09/13.
+//  Copyright (c) 2013 hentre. All rights reserved.
+//
+
+#import "NavViewController.h"
+
+@interface UserAccountViewController : NavViewController
+
+@end
