0b9d2585a581683c6e489811fc48a5830551abe5
HentreGeneral <hadoop@star-mac-pro.local>
2013-09-06 12:10:21 +0800

    c

---
+++ SmartHome/Message.m
@@ -0,0 +1,13 @@
+//
+//  Message.m
+//  SmartHome
+//
+//  Created by hadoop user account on 6/09/13.
+//  Copyright (c) 2013 hentre. All rights reserved.
+//
+
+#import "Message.h"
+
+@implementation Message
+
+@end
