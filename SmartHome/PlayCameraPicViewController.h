89c916c241e9dc621126d57739349e0a4b526870
yangyangduo <zhaoyang@vip.163.com>
2013-09-16 10:48:23 +0800

    new view controller

---
+++ SmartHome/PlayCameraPicViewController.h
@@ -0,0 +1,15 @@
+//
+//  PlayCameraPicViewController.h
+//  SmartHome
+//
+//  Created by Zhao yang on 9/16/13.
+//  Copyright (c) 2013 hentre. All rights reserved.
+//
+
+#import "NavViewController.h"
+
+@interface PlayCameraPicViewController : NavViewController
+
+@property (strong, nonatomic) NSArray *cameraPicPaths;
+
+@end
---
+++ SmartHome/PlayCameraPicViewController.m
@@ -0,0 +1,40 @@
+//
+//  PlayCameraPicViewController.m
+//  SmartHome
+//
+//  Created by Zhao yang on 9/16/13.
+//  Copyright (c) 2013 hentre. All rights reserved.
+//
+
+#import "PlayCameraPicViewController.h"
+
+@interface PlayCameraPicViewController ()
+
+@end
+
+@implementation PlayCameraPicViewController
+
+@synthesize cameraPicPaths;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
