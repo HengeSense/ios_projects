2450fa347333634ee3c03e4eb85bbfe28d156791
yangyangduo <zhaoyang@vip.163.com>
2013-09-25 11:38:51 +0800

    ff

---
+++ SmartHome/DeviceCommandBindingUnit.h
@@ -0,0 +1,13 @@
+//
+//  DeviceCommandBindingUnit.h
+//  SmartHome
+//
+//  Created by Zhao yang on 9/25/13.
+//  Copyright (c) 2013 hentre. All rights reserved.
+//
+
+#import "DeviceCommand.h"
+
+@interface DeviceCommandBindingUnit : DeviceCommand
+
+@end
---
+++ SmartHome/DeviceCommandBindingUnit.m
@@ -0,0 +1,13 @@
+//
+//  DeviceCommandBindingUnit.m
+//  SmartHome
+//
+//  Created by Zhao yang on 9/25/13.
+//  Copyright (c) 2013 hentre. All rights reserved.
+//
+
+#import "DeviceCommandBindingUnit.h"
+
+@implementation DeviceCommandBindingUnit
+
+@end
