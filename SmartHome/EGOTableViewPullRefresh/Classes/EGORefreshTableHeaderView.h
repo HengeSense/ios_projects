9d9de9ddde725abc352aa0da5ae7ba6cc410c1db
yangyangduo <zhaoyang@vip.163.com>
2013-11-22 15:30:56 +0800

    update

--- SmartHome/EGOTableViewPullRefresh/Classes/EGORefreshTableHeaderView.h
+++ SmartHome/EGOTableViewPullRefresh/Classes/EGORefreshTableHeaderView.h
@@ -36,7 +36,7 @@ typedef enum{
 #define DEFAULT_ARROW_IMAGE         [UIImage imageNamed:@"grayArrow.png"]
 #define DEFAULT_BACKGROUND_COLOR    [UIColor clearColor]
 #define DEFAULT_TEXT_COLOR          [UIColor colorWithRed:87.0/255.0 green:108.0/255.0 blue:137.0/255.0 alpha:1.0]
-#define DEFAULT_ACTIVITY_INDICATOR_STYLE    UIActivityIndicatorViewStyleGray
+#define DEFAULT_ACTIVITY_INDICATOR_STYLE    UIActivityIndicatorViewStyleWhite
 
 #define FLIP_ANIMATION_DURATION 0.18f
 
@@ -56,7 +56,6 @@ typedef enum{
     
     // Set this to Yes when egoRefreshTableHeaderDidTriggerRefresh delegate is called and No with egoRefreshScrollViewDataSourceDidFinishedLoading
     BOOL isLoading;
-	
 }
 
 @property(nonatomic,assign) id <EGORefreshTableHeaderDelegate> delegate;
